04e270d4de513a93428a0826cc91774eca23c24a
Bartlomiej <b.sopecki@gmail.com>
2019-08-27 09:04:28 +0200

    list items

--- resources/views/layouts/components/milestones.blade.php
+++ resources/views/layouts/components/milestones.blade.php
@@ -4,47 +4,47 @@
             <h2 class="milestones__title">Program milestones</h2>
         </header>
         <ul class="list">
-            <li class="list__part">
+            <li class="list__part list__part--left">
                 <div class="list__marker"></div>
                 <div class="list__content list__content--left">
                     <h3 class="list__subtitle">Zebranie i drużyny i treningi z football3</h3>
                     <p class="list__text">Pierwszym krokiem jest zawsze zebranie drużyny Fair Play złożonej z chłopców i dziewczyn oraz prowadzenie zajęć z Football3.</p>
                 </div>
             </li>
-            <li class="list__part">
+            <li class="list__part list__part--right">
                 <div class="list__marker"></div>
                 <div class="list__content list__content--right">
                     <h3 class="list__subtitle">Turniej Fair Play</h3>
                     <p class="list__text">Organizacja przez drużynę min. jednego turnieju Fair Play dla dzieci ze szkół podstawowych z wykorzystaniem metody Football3.</p>
                 </div>
             </li>
-            <li class="list__part">
+            <li class="list__part list__part--left">
                 <div class="list__marker"></div>
                 <div class="list__content list__content--left">
                     <h3 class="list__subtitle">„Zróbmy coś dobrego”</h3>
                     <p class="list__text">Zaplanowanie i realizacja przez drużynę dowolnej akcji charytatywnej lub pomocowej ukierunkowanej w stronę lokalnej społeczności.</p>
                 </div>
             </li>
-            <li class="list__part">
+            <li class="list__part list__part--right">
                 <div class="list__marker"></div>
                 <div class="list__content list__content--right">
                     <h3 class="list__subtitle">Ocena i punktacja</h3>
                     <p class="list__text">Wszystkie zadania realizowane przez drużyny na etapie 1 podlegają ocenie. 5 drużyn, które w najlepszy sposób wywiążą się ze swoich zadań, zakwalifikują się do etapu 2.</p>
                 </div>
             </li>
-            <li class="list__part">
+            <li class="list__part list__part--left">
                 <div class="list__marker"></div>
                 <div class="list__content list__content--left">
                     <h3 class="list__subtitle">Fair Play Football3 Festival</h3>
                     <p class="list__text">2-dniowy festiwal. Uczestnicy poznają się. Biorą udział w 2 dniowym turnieju football3. Pierwszy dzień 15 drużyn (drużyny mieszane), w 3 grupach. Do finału awansują 3 drużyny z każdej grupy. Dzień finałowy, to mecze w 3 grupach, po 3 drużyny. Do finałowej rozgrywki przechodzą najlepsze 3 drużyny. Finał wygrywa drużyna, która otrzymała najwięcej punktów, w tym punktów Fair Play.</p>
                 </div>
             </li>
-            <li class="list__part">
+            <li class="list__part list__part--special">
                 <div class="list__marker"></div>
-                <div class="list__content list__content--left">
+                <div class="list__content">
                     <h3 class="list__subtitle">Nowy Rok</h3>
                 </div>
-                <div class="list__content list__content--right">
+                <div class="list__content list__content--special">
                     <h3 class="list__subtitle list__subtitle--red">Nowy Początek</h3>
                 </div>
             </li>
